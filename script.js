8c94a9f0bf8f39721dd3445dd3ab22c7608cd42f
Ivan F <ifabrichnov@yandex.ru>
2020-04-03 13:15:13 +0300

    Poster, film card
    I made the posters active, when you go to that the movie card opens. Made a stub for films without a poster.

--- script.js
+++ script.js
@@ -3,37 +3,50 @@ const movie = document.querySelector('#movies');
 //начало url для обложки
 const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
-
 function apiSearch(e) {
   e.preventDefault();
-
   //определяем значение в input
   const searchText = document.querySelector('.form-control').value;
-  //api
-  const server = 'https://api.themoviedb.org/3/search/multi?api_key=54a1290145164c615ee7a1c5c2aa5ccd&language=ru&query=' + searchText;
-  movie.innerHTML = 'Загрузка';
+  //Условие, запрещающее вводить пустые запросы в строке
+  if (searchText.trim().length === 0) {
+    movie.innerHTML = '<h2 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h2>';
+    return;
+  }
+  //спиннер при загрузке страницы
+  movie.innerHTML = '<div class="spinner"></div>';
 
-  fetch(server).then(function (value) {
+  fetch('https://api.themoviedb.org/3/search/multi?api_key=54a1290145164c615ee7a1c5c2aa5ccd&language=ru&query=' + searchText).then(function (value) {
     if (value.status !== 200) {
       return Promise.reject(value);
     }
     return value.json();
   }).then(function (output) {
-
-
     let inner = '';
+    //условие, которое будет говорить о том, что по запросу ничего не найдено
+    if (output.results.length === 0) {
+      inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено.</h2>';
+    }
     //получаем названия фильмов или сериалов и добавляем в верстку
     output.results.forEach(function (item) {
       let nameItem = item.name || item.title;
+      //создаем заглушку постер, на случай если у фильма нет обложки
+      const poster = item.poster_path ? urlPoster + item.poster_path : './img/poster.jpg';
+      //добавляем новый атрибут dataInfo и добавляем его в верстку если тип person
+      let dataInfo = '';
+      if (item.media_type !== 'person') dataInfo = `data-id="${item.id} "data-type="${item.media_type}"`;
+      //верстка
       inner += `
          <div class="col-12 col-md-4 col-xl-3 item">
-         <img src="${urlPoster + item.poster_path}" alt="${nameItem}">
+         <img src="${poster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
          <h5>${nameItem}</h5>
          </div> 
       `;
     });
     //в контейнер movie передаем данные о фильме
     movie.innerHTML =  inner;
+
+    addEventMedia();
+
   }).catch(function (reason) {
     movie.innerHTML = 'Упс, что-то пошло не так!';
     console.error('error: ' + reason.status);
@@ -41,3 +54,89 @@ function apiSearch(e) {
 }
 
 searchForm.addEventListener('submit', apiSearch);
+
+//делаем постеры активными
+function addEventMedia() {
+  //определяем картики и навешиваем событие, чтоб при клике в дальнейшем открывалась инфа о фильме
+  //делаем активный клик только по фильмам и сериалам
+  const media = movie.querySelectorAll('img[data-id]');
+  //перебираем псевдомассив постеров и навешиваем обработчик события по клику
+  media.forEach(function (e) {
+    e.style.cursor = 'pointer';
+    e.addEventListener('click', showInfo);
+  });
+}
+
+//создадим функцию, которая будет выводить информацию о фильме при клике на постер
+function showInfo() {
+  let url = '';
+  if (this.dataset.type === 'movie') {
+    url = 'https://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=54a1290145164c615ee7a1c5c2aa5ccd&language=ru';
+  } else if (this.dataset.type === 'tv') {
+    url = 'https://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=54a1290145164c615ee7a1c5c2aa5ccd&language=ru';
+  } else {
+    movie.innerHTML = '<h2 class="col-12 text-center text-danger">Произошла ошибка, повторите позже.</h2>';
+  }
+  fetch(url).then(function (value) {
+    if (value.status !== 200) {
+      return Promise.reject(value);
+    }
+    return value.json();
+  }).then(function (output) {
+    console.log(output);
+    movie.innerHTML = `
+    <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+    <div class="col-4">
+        <img src="${urlPoster + output.poster_path}" alt="${output.name || output.title}">
+        ${(output.homepage) ? `<p class="text-center"> <a href="${output.homepage}"  target="_blank">Официальная страница</a> </p>` : ''} 
+        ${(output.imdb_id) ? `<p class="text-center"> <a href="https://imdb.com/title/${output.imdb_id}" target="_blank">Страница на IMDB</a> </p>` : ''} 
+    </div>
+    <div class="col-8">
+        <p>Рейтинг: ${output.vote_average} </p>
+        <p>Статус: ${output.status} </p>
+        <p>Премьера: ${output.first_air_date || output.release_date} </p>
+        
+        ${(output.last_episode_to_air) ? `<p>${output.number_of_seasons} сезон(ов) ${output.last_episode_to_air.episode_number} серий вышло</p>` : ''}
+        
+        <p>Описание: ${output.overview} </p>
+    </div>
+    `;
+  }).catch(function (reason) {
+    movie.innerHTML = 'Упс, что-то пошло не так!';
+    console.error('error: ' + reason.status);
+  });
+}
+
+//выдаем список топ фильмов недели при завершении загрузки страницы
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://api.themoviedb.org/3/trending/all/week?api_key=54a1290145164c615ee7a1c5c2aa5ccd&language=ru')
+    .then(function (value) {
+    if (value.status !== 200) {
+      return Promise.reject(value);
+    }
+    return value.json();
+  }).then(function (output) {
+    let inner = '<h4 class="col-12 text-center text-info">Топ фильмов недели:</h4>';
+    if (output.results.length === 0) {
+      inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено.</h2>';
+    }
+    output.results.forEach(function (item) {
+      let nameItem = item.name || item.title;
+      //будем проверять title для передачи его атрибут верстки
+      let mediaType = item.title ? 'movie' : 'tv';
+      const poster = item.poster_path ? urlPoster + item.poster_path : './img/poster.jpg';
+      let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+      inner += `
+         <div class="col-12 col-md-4 col-xl-3 item">
+         <img src="${poster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
+         <h5>${nameItem}</h5>
+         </div> 
+      `;
+    });
+    movie.innerHTML =  inner;
+    addEventMedia();
+  }).catch(function (reason) {
+    movie.innerHTML = 'Упс, что-то пошло не так!';
+    console.error('error: ' + reason.status);
+  });
+});
